acfb6af3bdaa9f70d2b7b8ec415135ef0f43e9a8
adibsadeghi1993 <adibsadeghi7272@gmail.com>
2024-07-02 00:54:47 +0330

    adding loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center">
+      <img src="spiner.svg" alt="loading..." />
+    </div>
+  );
+}
