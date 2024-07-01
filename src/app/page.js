57060a0bd8d367b74133ed7873ca1166fd04ec7a
adibsadeghi1993 <adibsadeghi7272@gmail.com>
2024-07-02 00:14:36 +0330

    fetch data from tmdb

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,26 @@
-import React from "react";
+import Results from "@/components/Results";
 
-export default function Home() {
-  return <div>home</div>;
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=61485c7386a2582989a6d80350934ff1&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error("something went wrong");
+  }
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
